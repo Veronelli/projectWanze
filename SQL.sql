620d4569fce54cbaf0efff92f9e1e45fd4acadbc
Veronelli <facu2000veronelli@outlook.com>
2020-08-29 20:37:06 -0300

    I changed the parameters in the SQL file

--- SQL.sql
+++ SQL.sql
@@ -2,31 +2,32 @@
 
 --GitHub:Veronelli,
 
---  PARAMETERS: proyect_wanze || TABLES: client, mechanic, car, turn, service, status, task || ID: id_client, id_mechanic, id_car, id_turn, id_service, id_status, id_task
+--  PARAMETERS: proyect_wanze || TABLES: clients, mechanics, cars, turns, services, status, tasks || ID: id_client, id_mechanic, id_car, id_turn, id_service, id_status, id_task
 
 CREATE DATABASE proyect_wanze;
 
 USE proyect_wanze;
 
 --Client
-CREATE TABLE client(
+CREATE TABLE clients(
 
     id_client INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     last_name varchar(20),
     ID varchar(8),
-    MAIL varchar(30),|
+    MAIL varchar(30),
     Cel_Phone varchar(12)
 
 );
 --INSERT INTO client (last_name, ID, MAIL, Cel_Phone) values ('Facundo','99277390','facu2000veronelli@outlook.com','113245502')
 
 --Turn
-CREATE TABLE turn(
+CREATE TABLE turns(
 
     id_turn INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    id_car INT NOT NULL FOREIGN KEY car(id_car),
-    id_mechanic INT NOT NULL FOREIGN KEY mechanic(id_mechanic),
-    id_client INT NOT NULL FOREIGN KEY client(id_client),
+    FOREIGN KEY fk_car_id REFERENCES cars(id_car),
+    FOREIGN KEY fk_mechanic_id REFERENCES mechanics(id_mechanic),
+    FOREIGN KEY fk_client_id REFERENCES clients(id_client),
+
     _date DATE,
     _time Time,
 
